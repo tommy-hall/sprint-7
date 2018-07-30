23728adfde579c0080286a6e9fbd7bb6ae917bf0
tommy-hall <thall2906@gmail.com>
2018-07-31 11:06:48 +1200

    Chapter 3 Solutions

---
+++ eloquent.js
@@ -0,0 +1,56 @@
+// Chapter 3 - Functions
+
+
+// 3.1 Minimum 
+
+function min(a, b) {
+    if (a < b) return a;
+    else return b;
+  }
+  
+  console.log(min(0, 10));
+  // → 0
+  console.log(min(0, -10));
+  // → -10
+
+
+  // 3.2 Recursion
+
+  function isEven(n) {
+    if (n == 0) return true;
+    else if (n == 1) return false;
+    else if (n < 0) return isEven(-n);
+    else return isEven(n - 2);
+  }
+  
+  console.log(isEven(50));
+  // → true
+  console.log(isEven(75));
+  // → false
+  console.log(isEven(-1));
+  // → false
+
+
+  // 3.3 Bean Counting
+
+  function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == ch) {
+        counted += 1;
+      }
+    }
+    return counted;
+  }
+  
+  function countBs(string) {
+    return countChar(string, "B");
+  }
+  
+  console.log(countBs("BBC"));
+  // → 2
+  console.log(countChar("kakkerlak", "k"));
+  // → 4
+
+
+
